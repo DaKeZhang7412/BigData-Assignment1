3818faf1060413916e62ef3313e6caff54b0808e
Neo_Zhang_JLY <neo.zhang@jlyproductions.com>
2020-02-23 21:03:41 -0600

    input test

--- rowCount.py
+++ rowCount.py
@@ -2,16 +2,17 @@ import sys
 import StringIO
 import os
 
-def rowCount():
+def mapper(test_data_directory):
 
 	lineCount = 0
 	fileCount = 0
 	
 	# Data location
-	data_directory = ''
-	sys.stdin = StringIO.StringIO(data_directory)
+	# dataDirectory = 'testData'
+	# dataDirectory = 'data'
 
-	for subdir, dirs, files in os.walk(data_directory):
+	for subdir, dirs, files in os.walk(input("Enter data directory: ")):
+
 		for fileName in files:
 			filePath = subdir + os.sep + fileName	
 			
@@ -26,13 +27,14 @@ def rowCount():
 				# Closing files
 				file.close() 
 	
-	print("Total Row Count for {} Directory is {}".format(data_directory, lineCount))
-	print("Total File Count for {} Directory is {}".format(data_directory, fileCount))
+	print("Total Row Count for {} Directory is {}".format(dataDirectory, lineCount))
+	print("Total File Count for {} Directory is {}".format(dataDirectory, fileCount))
 
 
 # This function allows you to test the mapper with the provided test file
 def main():
-    rowCount()
+    # sys.stdin = StringIO.StringIO(test_data_directory)
+    mapper(test_data_directory)
     # sys.stdin = sys.__stdin__
 
 if __name__ == "__main__":
